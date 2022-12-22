2624b523faab57f5d662c5af3fb4d25eb4d4726c
manha <manhamsterdam2003@gmail.com>
2022-12-22 13:17:36 +0700

    tạo ghi chú

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
+# 1. tạo 1 thư mục gốc chứa repo và các file .py chứa mã code
+# 2. Add file .py vào repository
+# 3. Sửa file .py xong thì commit
+# 4. Sửa ok rồi thì push lên github
 print("gym")
 print("happy")
 print("Here we go.")
