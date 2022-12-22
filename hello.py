73336b8aac1ece02fecf0dffdd8a66cb4f0719ee
manha <manhamsterdam2003@gmail.com>
2022-12-22 13:25:34 +0700

    local, remote

--- hello.py
+++ hello.py
@@ -2,6 +2,9 @@
 # 2. Add file .py vào repository
 # 3. Sửa file .py xong thì commit
 # 4. Sửa ok rồi thì push lên github
+
+# Local: { 1 repository, several branches} ---push-->
+# Remote:{ 1 repository, 1 folder contain branches( origin))
 print("gym")
 print("happy")
 print("Here we go.")
